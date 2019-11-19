5061a44507723a7c49f9968ffe50405781bb5203
Martin Vicknair <martinvicknair@gmail.com>
2019-11-19 10:53:08 -0700

    Updates

---
+++ precache-manifest.fd735c829d5f5dba9236366b4da3c9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff8aeec975ccbae3516652e664714c9",
+    "url": "/react-menuboard/index.html"
+  },
+  {
+    "revision": "27d07b7fb1ea088cd83c",
+    "url": "/react-menuboard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4946cd39c9c3b690f3f4",
+    "url": "/react-menuboard/static/js/2.2abefce3.chunk.js"
+  },
+  {
+    "revision": "27d07b7fb1ea088cd83c",
+    "url": "/react-menuboard/static/js/main.596aeca8.chunk.js"
+  },
+  {
+    "revision": "6440d4d2fad73ec07c92",
+    "url": "/react-menuboard/static/js/runtime-main.3e3c78c2.js"
+  },
+  {
+    "revision": "947093ea8f8fcabf2c5f2a484edbcf3f",
+    "url": "/react-menuboard/static/media/logo.947093ea.png"
+  }
+]);
